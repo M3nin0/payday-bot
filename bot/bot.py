94855976d2118f29700f4fee296601ec29f884bb
Felipe Menino <efelipecarlos@gmail.com>
2018-01-03 15:53:12 -0200

    Adição do sistema de notificação de faturas em atraso

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,15 @@
 import logging
 import telegram
+import datetime
+
 from time import sleep
 from toolbox import ToolBox
-from telegram.error import Unauthorized
-from telegram.error import NetworkError
+from threading import Thread
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from telegram.ext import CommandHandler
 
 class Bot(object):
     def __init__(self):
@@ -45,16 +49,40 @@ class Bot(object):
 
             table = ToolBox.connect_api(str(file_id) + '.json', 'faturas')
             if table == -1:
-                print(table)
                 update.message.reply_text('Erro ao tentar se conectar com a planilha de gastos, insira o arquivo novamente')
             else:
-                print(table)
-                update.message.reply_text('Pronto! Agora já tenho acesso a suas faturas, logo menos estarei verificando e enviando notificações para você')
+                update.message.reply_text('Pronto! Agora já tenho acesso a suas faturas, logo menos estarei verificando e enviando notificações sobre suas faturas para você')
+
+                # Cria thread para evitar que o bot fique inutilizavel durante o processo de verificação
+                thread = Thread(target = self.send_notify, args = (bot, update, table))
+                thread.start()
+
         except BaseException as e:
             print(e)
 
-    def send_notify(self, bot, update):
-        pass
+    def send_notify(self, bot, update, table):
+        # Faz verificação e envia notificação se necessário
+        while True:
+            update.message.reply_text('Estou fazendo a verificação de suas faturas agora')
+            itens_table = table.get_all_values()[1:]
+            # Data do dia atual
+            hoje = datetime.date.today()
+            for fatura in itens_table:
+                if fatura[-1] != 'Fechado':
+                    vencimento = fatura[2].split('-')[::-1]
+                    data_vencimento = datetime.date(int(vencimento[0]),
+                                                    int(vencimento[1]),
+                                                    int(vencimento[2]))
+                    atraso = (hoje - data_vencimento).days
+                    if atraso > int(fatura[-2]):
+                        update.message.reply_text('A seguinte fatura precisa ser paga: \n ' +
+                                                  '- Data de emissão: ' + fatura[1] +
+                                                  '\n - Data de vencimento: ' + fatura[2] +
+                                                  '\n - Nome da empresa: ' + fatura[3] +
+                                                  '\n - Valor da conta: ' + fatura[4] +
+                                                '\n - Status da fatura: ' + fatura[6])
+            # Entra em espera durante um dia
+            sleep(86400)
 
 if __name__ == '__main__':
     Bot()
